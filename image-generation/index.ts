3db8c6494bcbf296106c6cece3e9dc042b3fa7a4
Khurdhula-Harshavardhan <khurdhulaharshavardhan@gmail.com>
2025-03-03 23:25:35 -0800

    updating type to text, to fit smithery response type.

--- image-generation/index.ts
+++ image-generation/index.ts
@@ -155,12 +155,12 @@ server.setRequestHandler(CallToolRequestSchema, async (request) => {
         const base64Image = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`;
         
         //return ImageContent
-        const ImageContent = {
-          type: "image",
-          base64: base64Image,
+        const content = {
+          type: "text",
+          "base64": base64Image,
           mimeType: "image/png",
         };
-        return { ImageContent };
+        return { content };
       } catch (error: any) {
         console.error("Error processing IMAGE_GENERATION request:", error);
         const content = [{
