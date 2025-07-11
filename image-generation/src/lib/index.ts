874942f5672d2ec6d65d0942df4e69f3177d4efb
jigsawstack <91052168+VineeTagarwaL-code@users.noreply.github.com>
2025-07-11 22:53:12 +0530

    feat: updated smithery.yaml

--- image-generation/src/lib/index.ts
+++ image-generation/src/lib/index.ts
@@ -5,9 +5,9 @@ dotenv.config();
 
 const JIGSAWSTACK_API_KEY = process.env.JIGSAWSTACK_API_KEY;
 
-if (!JIGSAWSTACK_API_KEY) {
-  throw new Error("JIGSAWSTACK_API_KEY environment variable is required");
-}
+// if (!JIGSAWSTACK_API_KEY) {
+//   throw new Error("JIGSAWSTACK_API_KEY environment variable is required");
+// }
 
 const jigsawStackClient = JigsawStack({
   apiKey: JIGSAWSTACK_API_KEY,
